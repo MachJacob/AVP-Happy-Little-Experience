288d543333b51f731c3e78f20655aa952dac2027
Jacob Carr <jcarr343@hotmail.co.uk>
2020-01-23 13:26:45 +0000

    Some Scene stuff, more VR Support

---
+++ A Happy Little Experience/Assets/Oculus/VR/Scripts/SceneChanger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(OVRInput.GetDown(OVRInput.Button.Two) || Input.GetKeyDown(KeyCode.Return))
+        {
+            int count = SceneManager.sceneCountInBuildSettings;
+            int index = SceneManager.GetActiveScene().buildIndex;
+
+            if((index+1) >= count)
+            { index = 0; }
+
+            SceneManager.LoadScene(index + 1);
+        }
+    }
+}
