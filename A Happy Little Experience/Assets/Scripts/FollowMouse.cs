1df5f19fdb19ad53b51c44dcbf3436204bd475eb
Kai Tooley <kai.p.o.tooley@gmail.com>
2020-01-30 15:26:28 +0000

    collision based drawing
    added drawing demo scene
    updated follow mouse to work in perspective mode
    modified drawable class to change pixels when colided with
    
    added things to controler output log

---
+++ A Happy Little Experience/Assets/Scripts/FollowMouse.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+    public Vector3 mouse_world_position = new Vector3();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouse_world_position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 5.8f));
+
+        transform.position = new Vector3(mouse_world_position.x, mouse_world_position.y, -5.6f);
+    }
+}
