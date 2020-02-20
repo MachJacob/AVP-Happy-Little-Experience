6964ea190f8f50ae9468cec0156eafd82fc9cdd6
Jacob Carr <jcarr343@hotmail.co.uk>
2020-02-20 11:27:11 +0000

    Palette grabbing
    Still minor bugs

---
+++ A Happy Little Experience/Assets/Grabber.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grabber : MonoBehaviour
+{
+    public bool grab;
+    public GameObject held;
+    private bool right;
+
+    void Start()
+    {
+        grab = false;
+        if (gameObject.name.Contains("Left"))
+        {
+            right = false;
+        }
+        else
+        {
+            right = true;
+        }
+        held = null;
+    }
+
+    void Update()
+    {
+        if (held && right && (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger) || Input.GetKeyDown(KeyCode.Backspace)) && !grab)
+        {
+            held.GetComponent<GrabBrush>().Drop();
+            held = null;
+        }
+        else if (held && !right && (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) || Input.GetKeyDown(KeyCode.Return)) && !grab)
+        {
+            held.GetComponent<GrabBrush>().Drop();
+            held = null;
+        }
+        grab = false;
+    }
+
+    public bool Grab()
+    {
+        if (right)
+        {
+            grab = true;
+            return (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger) || Input.GetKeyDown(KeyCode.Backspace));
+        }
+        else
+        {
+            grab = true;
+            return (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) || Input.GetKeyDown(KeyCode.Return));
+        }
+    }
+}
--- A Happy Little Experience/Assets/Scripts/grabBrush.cs
+++ A Happy Little Experience/Assets/Scripts/grabBrush.cs
@@ -2,54 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class grabBrush : MonoBehaviour
+public class GrabBrush : MonoBehaviour
 {
     private GameObject childController;
     private bool held;
     [SerializeField] private GameObject[] colliding;
-    public float brushOffset = 0.1f;
+    public float rotationOffset;
+    public Vector3 offset;
+    private bool drop;
     // Start is called before the first frame update
     void Start()
     {
         colliding = new GameObject[2];
+        drop = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if ((OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) || Input.GetKeyDown(KeyCode.Return) || 
-            (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger) || Input.GetKeyDown(KeyCode.Backspace))) && held)
-        {
-            childController.SetActive(true);
-            transform.parent = null;
-            held = false;
-            transform.position = childController.transform.position + Vector3.forward * brushOffset;
-            childController = null;
-            GetComponent<Rigidbody>().useGravity = true;
-            GetComponent<Rigidbody>().isKinematic = false;
-        }
-        else if ((OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) || Input.GetKeyDown(KeyCode.Return)) && !held && colliding[0])      //attach left
+        if ((OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) || Input.GetKeyDown(KeyCode.Return)) && !held && colliding[0] && !drop)           //attach left
         {
             transform.parent = colliding[0].transform;
             childController = colliding[0].transform.GetChild(0).gameObject;
-            transform.localRotation = Quaternion.AngleAxis(90, Vector3.right);
+            transform.localRotation = Quaternion.AngleAxis(rotationOffset, Vector3.right);
             //childController.SetActive(false);
             held = true;
-            transform.localPosition = Vector3.forward * brushOffset;
+            transform.localPosition = offset;
             GetComponent<Rigidbody>().useGravity = false;
             GetComponent<Rigidbody>().isKinematic = true;
+            colliding[0].GetComponent<Grabber>().held = gameObject;
         }
-        else if ((OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger) || Input.GetKeyDown(KeyCode.Backspace)) && !held && colliding[1]) //attach right
+        else if ((OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger) || Input.GetKeyDown(KeyCode.Backspace)) && !held && colliding[1] && !drop) //attach right
         {
             transform.parent = colliding[1].transform;
             childController = colliding[1].transform.GetChild(0).gameObject;
-            transform.localRotation = Quaternion.AngleAxis(90, Vector3.right);
+            transform.localRotation = Quaternion.AngleAxis(rotationOffset, Vector3.right);
             //childController.SetActive(false);
             held = true;
-            transform.localPosition = Vector3.forward * brushOffset;
+            transform.localPosition = offset;
             GetComponent<Rigidbody>().useGravity = false;
             GetComponent<Rigidbody>().isKinematic = true;
+            colliding[1].GetComponent<Grabber>().held = gameObject;
         }
+        drop = false;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -79,5 +74,17 @@ public class grabBrush : MonoBehaviour
                 colliding[1] = null;
             }
         }
+    }
+
+    public void Drop()
+    {
+        childController.SetActive(true);
+        transform.parent = null;
+        held = false;
+        transform.position = childController.transform.position + offset;
+        childController = null;
+        GetComponent<Rigidbody>().useGravity = true;
+        GetComponent<Rigidbody>().isKinematic = false;
+        drop = true;
     }
 }
