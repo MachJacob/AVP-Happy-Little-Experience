5d8e000f1a67af14f9c017010f4be0176f0c89a2
Kai Tooley <kai.p.o.tooley@gmail.com>
2020-03-12 19:13:11 +0000

    started book
    book changes page on trigger tab
    layed ut basic menu with credits
    added text mesh pro to the project

---
+++ A Happy Little Experience/Assets/Scripts/Book/PageControlle.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PageControlle : MonoBehaviour
+{
+    public enum PAGES
+    {
+        MENU,
+        STAMPS,
+        SAVELOAD,
+        CLOURPRESETS
+    }
+
+    public GameObject menu;
+    public GameObject stamps;
+    public GameObject saveload;
+    public GameObject colourpresets;
+
+    public void OpenPage(PAGES openTo)
+    {
+        //deactivate all pages
+        menu.SetActive(false);
+        stamps.SetActive(false);
+        saveload.SetActive(false);
+        colourpresets.SetActive(false);
+        //reactivate only the required page
+        switch (openTo)
+        {
+            case PAGES.MENU:
+                menu.SetActive(true);
+                break;
+            case PAGES.STAMPS:
+                stamps.SetActive(true);
+                break;
+            case PAGES.SAVELOAD:
+                saveload.SetActive(true);
+                break;
+            case PAGES.CLOURPRESETS:
+                colourpresets.SetActive(true);
+                break;
+            default:
+                menu.SetActive(true);
+                break;
+        }
+    }
+
+    private void Start()
+    {
+        //Activate only the menu
+        menu.SetActive(true);
+        stamps.SetActive(false);
+        saveload.SetActive(false);
+        colourpresets.SetActive(false);
+    }
+}
