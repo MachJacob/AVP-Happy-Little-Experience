af090d44deac770b7abe50f964f6a8ed600b9667
Aaron Drinkwater <Aaron3.Drinkwater@live.uwe.ac.uk>
2020-03-26 10:39:13 +0000

    Save stuff added back in for no error.

--- A Happy Little Experience/Assets/Scripts/JSON/Save.cs
+++ A Happy Little Experience/Assets/Scripts/JSON/Save.cs
@@ -5,26 +5,39 @@ using UnityEngine;
 [System.Serializable]
 public class Save
 {
-    public string name;
+    public int something;
+    public int something_else;
+
     public int id;
+    public Vector3 g_positions;
+    public float g_poisions_x;
+    public float g_poisions_y;
+    public GameObject g_object;
 
-    public Transform the_transform;
-    public Vector3 position;
-    public Quaternion rotation;
-    public Vector3 scale;
 
+
+    //test stuff
+    public int coinsNum;
+    public int diamondsNum;
 
     public float playerPositionX;
     public float playerPositionY;
 
+    //MARKER If you want to save the enemy position 
+    public List<float> enemyPositionX = new List<float>();
+    public List<float> enemyPositionY = new List<float>();
+    public List<bool> isDead = new List<bool>();
 
-    public void init(int _id, Transform _transform, Vector3 _position, Quaternion _rotation, Vector3 _scale)
-    {
-        id = _id;
-        the_transform = _transform;
-        position = _position;
-        rotation = _rotation;
-        scale = _scale;
+    public List<float> g_object_pos = new List<float>();
 
-    }
+
 }
+
+public class SavePos
+{
+
+    public List<float> g_object_pos = new List<float>();
+
+}
+
+
